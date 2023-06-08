fb1192237447ed41b660abf413b303ba5bbd80c8
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-08 20:27:31 +0530

    Update selectController.js

--- controllers/selectController.js
+++ controllers/selectController.js
@@ -1,5 +1,6 @@
 const { validationResult } = require("express-validator");
 
+// /select - buyer app specifies the items & quantity selected by a buyer;
 const select = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -8,7 +9,40 @@ const select = async (req, res) => {
 
   const { context, message } = req.body;
 
-  res.status(202).json("Select");
-};
+  // Prepare the response payload
+  const response = {
+    context: {
+      domain: context.domain,
+      country: context.country,
+      city: context.city,
+      action: context.action,
+      core_version: context.core_version,
+      bap_id: context.bap_id,
+      bap_uri: context.bap_uri,
+      transaction_id: context.transaction_id,
+      message_id: context.message_id,
+      timestamp: context.timestamp,
+      ttl: context.ttl,
+    },
+    message: {
+      sellers: sellersInCity,
+      finder_fee: finderFee,
+    },
+  };
 
+  const responseData = await axios.post(
+    "https://virtserver.swaggerhub.com/ONDCTech/ONDC-Protocol-Core/1.0.0/select",
+    response,
+    {
+      headers: {
+        Authorization: "iUTpWtF68yckymVVY/aaXPHrMMPRz/dvYhXf3leVRI8=",
+      },
+    }
+  );
+
+  // Send the response
+  console.log(responseData);
+
+  res.status(202).json(responseData.data);
+};
 exports.select = select;
