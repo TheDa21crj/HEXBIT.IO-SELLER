7ab02e91de5d930aa26659ddd1242f72b13707d1
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-08 23:58:22 +0530

    Update selectController.js

--- controllers/selectController.js
+++ controllers/selectController.js
@@ -55,6 +55,11 @@ const select = async (req, res) => {
 };
 
 const onSelect = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   res.status(202).json("responseData.data");
 };
 
