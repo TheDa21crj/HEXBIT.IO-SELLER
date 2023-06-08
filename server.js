4e01f0e77bdd27b0cc24b3e6e22a11dc27c4848b
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-09 02:43:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ app.get("/test", (req, res) => {
 });
 
 // route
-app.use("/api/User", require("./routes/Login"));
 app.use("/api/Core", require("./routes/Core"));
+app.use("/api/User", require("./routes/Login"));
 
 app.use((req, res, next) => {
   console.log(req.url);
