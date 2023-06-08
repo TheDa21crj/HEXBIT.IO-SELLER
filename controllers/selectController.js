686859c70c9edd37cc3526b0681b90d27933ea0c
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-08 20:10:12 +0530

    Update selectController.js

--- controllers/selectController.js
+++ controllers/selectController.js
@@ -1,6 +1,13 @@
 const { validationResult } = require("express-validator");
 
 const select = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { context, message } = req.body;
+
   res.status(202).json("Select");
 };
 
