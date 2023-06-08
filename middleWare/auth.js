2159c5d5bdc143b7a87f1bb769550e81e3beeefb
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-09 02:59:02 +0530

    Update auth.js

--- middleWare/auth.js
+++ middleWare/auth.js
@@ -1,23 +1,26 @@
-const jwt = require('jsonwebtoken');
-const HttpError = require('../models/HttpError')
+const jwt = require("jsonwebtoken");
+const HttpError = require("../models/HttpError");
 // const HttpError = require('../models/http-error');
 
 module.exports = (req, res, next) => {
-  if (req.method === 'OPTIONS') {
+  if (req.method === "OPTIONS") {
     return next();
   }
   try {
     const token = req.headers.authorization; // Authorization: 'Bearer TOKEN'
 
     if (!token) {
-      const error = new HttpError('Authentication failed!',401);
-      return next(error)
+      const error = new HttpError("Authentication failed!", 401);
+      return next(error);
     }
-    const decodedToken = jwt.verify(token,process.env.JWT_SECRATE);
-    res.locals.userData = { userEmail: decodedToken.userEmail , userLevel:decodedToken.designation};
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRATE);
+    res.locals.userData = {
+      userEmail: decodedToken.userEmail,
+      userLevel: decodedToken.designation,
+    };
     next();
   } catch (err) {
-    const error = new HttpError('Authentication failed!', 403);
+    const error = new HttpError("Authentication failed!", 403);
     console.log(err);
     return next(error);
   }
