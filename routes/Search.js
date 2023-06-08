8213d71b55567bb43284bca44229ade9321ed70a
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-08 18:05:25 +0530

    Update Search.js

--- routes/Search.js
+++ routes/Search.js
@@ -1,88 +1,12 @@
 const express = require("express");
-const xlsx = require("xlsx");
-const path = require("path");
-const axios = require("axios");
 
 const router = express.Router();
 
-// Define the route for the /search API
-router.post("/", async (req, res) => {
-  const filePath = path.join(
-    __dirname,
-    "..",
-    "Data",
-    "ONDC-city-state-codes.xlsx"
-  );
+// controllers
+const searchController = require("../controllers/searchController");
 
-  const workbook = xlsx.readFile(filePath);
-
-  const sheet = workbook.Sheets[workbook.SheetNames[0]];
-  const cityCodes = xlsx.utils.sheet_to_json(sheet, { header: 1 });
-
-  // Extract the necessary data from the request payload
-  const { context, message } = req.body;
-  const { city } = context;
-
-  // Perform the search operation based on the city
-  const sellersInCity = searchSellersByCity(city, cityCodes);
-
-  // Process buyer app finder fee calculations
-  const finderFeeType =
-    message.intent.payment["@ondc/org/buyer_app_finder_fee_type"];
-  const finderFeeAmount =
-    message.intent.payment["@ondc/org/buyer_app_finder_fee_amount"];
-  const finderFee = calculateFinderFee(finderFeeType, finderFeeAmount);
-
-  // Prepare the response payload
-  const response = {
-    context: {
-      domain: context.domain,
-      country: context.country,
-      city: context.city,
-      action: context.action,
-      core_version: context.core_version,
-      bap_id: context.bap_id,
-      bap_uri: context.bap_uri,
-      transaction_id: context.transaction_id,
-      message_id: context.message_id,
-      timestamp: context.timestamp,
-      ttl: context.ttl,
-    },
-    message: {
-      sellers: sellersInCity,
-      finder_fee: finderFee,
-    },
-  };
-
-  const responseData = await axios.post(
-    "https://virtserver.swaggerhub.com/ONDCTech/ONDC-Protocol-Core/1.0.0/search",
-    {
-      headers: {
-        Authorization: "iUTpWtF68yckymVVY/aaXPHrMMPRz/dvYhXf3leVRI8=",
-      },
-    }
-  );
-
-  // Send the response
-  console.log(responseData);
-
-  res.status(202).json(responseData.data);
-});
-
-// Helper function to search for sellers in the given city
-function searchSellersByCity(city, cityCodes) {
-  // Perform the search logic based on the city
-  const sellersInCity = []; // Placeholder, implement your actual search logic here
-
-  return sellersInCity;
-}
-
-// Helper function to calculate the buyer app finder fee
-function calculateFinderFee(type, amount) {
-  // Perform the finder fee calculation logic based on the type and amount
-  const finderFee = {}; // Placeholder, implement your actual calculation logic here
-
-  return finderFee;
-}
+// search
+// const search = async (req, res, next) => {};
+router.post("/", searchController.search);
 
 module.exports = router;
