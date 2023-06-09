1835f3570f0b912d2771ecb3231b2f450d451c5a
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-09 12:11:37 +0530

    Update ststuswithinit&confirm.js

--- routes/ststuswithinit&confirm.js
+++ routes/ststuswithinit&confirm.js
@@ -9,4 +9,8 @@ const ststuswithinit = require("../controllers/ststuswithinit&confirm");
 // Endpoint for /on_init
 router.post("/on_init", ststuswithinit.on_init);
 
+// Endpoint for /confirm
+router.post("/confirm", ststuswithinit.confirm);
+router.post("/status", ststuswithinit.status);
+
 module.exports = router;
