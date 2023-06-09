bba59d38b4553730c5aa1f6310bae7cd34ad7587
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-09 12:17:06 +0530

    Update Core.js

--- routes/Core.js
+++ routes/Core.js
@@ -6,6 +6,7 @@ const router = express.Router();
 // controllers
 const searchController = require("../controllers/searchController");
 const selectController = require("../controllers/selectController");
+const supportController = require("../controllers/support");
 
 // search
 router.post(
@@ -22,5 +23,8 @@ router.post(
   check("message", "message is Required").not().isEmpty(),
   selectController.select
 );
+
+// support
+router.post("/support", supportController.support);
 
 module.exports = router;
