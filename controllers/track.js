a6a269f683fad204036865eba002064ec2a33f3d
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-09 12:17:45 +0530

    Update track.js

--- controllers/track.js
+++ controllers/track.js
@@ -1,19 +1,24 @@
 const axios = require("axios");
 const { validationResult } = require("express-validator");
 
-// Endpoint for handling support requests
-const support = async (req, res) => {
-  // Extract the reference ID from the request body
-  const { ref_id } = req.body.message;
+// Endpoint for handling track requests
+const track = async (req, res) => {
+  app.post("/track", (req, res) => {
+    // Extract the order ID and callback URL from the request body
+    const { order_id, callback_url } = req.body.message;
 
-  // Perform necessary validations and checks on the reference ID
-  // ...
+    // Perform necessary validations and checks on the order ID and callback URL
+    // ...
 
-  // Call the support function with the reference ID
-  const supportResponse = handleSupportRequest(ref_id);
+    // Call the track function with the order ID and callback URL
+    trackOrder(order_id, callback_url);
 
-  // Return the support response
-  res.json({ success: true, message: supportResponse });
+    // Return a success response
+    res.json({
+      success: true,
+      message: "Order tracking initiated successfully",
+    });
+  });
 };
 
-exports.support = support;
+exports.track = track;
