84d8375432c4fb70b06cf55b2fbeba64df8f1085
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-09 12:04:09 +0530

    Update confirmwithCOD.js

--- routes/confirmwithCOD.js
+++ routes/confirmwithCOD.js
@@ -8,5 +8,10 @@ const confirmwithCOD = require("../controllers/confirmwithCOD");
 
 // Endpoint for /on_search
 router.post("/on_search", confirmwithCOD.on_search);
+router.post("/select", confirmwithCOD.select);
+router.post("/search", confirmwithCOD.search);
+router.post("/confirm", confirmwithCOD.confirm);
+router.post("/on_confirm", confirmwithCOD.on_confirm);
+router.post(["/on_status", "/on_update"], confirmwithCOD.on_status);
 
 module.exports = router;
