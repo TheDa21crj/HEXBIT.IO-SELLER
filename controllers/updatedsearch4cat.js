740476173eeb61febb013d78d313ac890541b8d8
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-09 12:30:30 +0530

    Update updatedsearch4cat.js

--- controllers/updatedsearch4cat.js
+++ controllers/updatedsearch4cat.js
@@ -62,7 +62,23 @@ const searchCategory = async (req, res) => {
 };
 
 // Search by fulfillment end location
-const searchFulfillment = async (req, res) => {};
+const searchFulfillment = async (req, res) => {
+  const { fulfillment, payment } = req.body.message.intent;
+
+  const gps = fulfillment.end.location.gps;
+  const areaCode = fulfillment.end.location.address.area_code;
+  const finderFeeType = payment["@ondc/org/buyer_app_finder_fee_type"];
+  const finderFeeAmount = payment["@ondc/org/buyer_app_finder_fee_amount"];
+
+  // Perform search logic based on GPS and area code
+  const searchResults = performSearchByFulfillment(gps, areaCode);
+
+  // Calculate finder fee based on the finder fee type and amount
+  const finderFee = calculateFinderFee(finderFeeType, finderFeeAmount);
+
+  // Return search results and finder fee
+  res.json({ searchResults, finderFee });
+};
 
 exports.searchFulfillment = searchFulfillment;
 exports.searchCategory = searchCategory;
