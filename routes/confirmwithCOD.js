5d65e75f447ed553598d5f384eed80bbeaeefca6
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-10 01:41:24 +0530

    Update confirmwithCOD.js

--- routes/confirmwithCOD.js
+++ routes/confirmwithCOD.js
@@ -6,14 +6,15 @@ const router = express.Router();
 // controllers
 const confirmwithCOD = require("../controllers/confirmwithCOD");
 
+// Endpoint for /search
+router.post("/search", confirmwithCOD.search);
+
 // Endpoint for /on_search
 router.post("/on_search", confirmwithCOD.on_search);
 
 // Endpoint for /select
 router.post("/select", confirmwithCOD.select);
 
-// Endpoint for /search
-router.post("/search", confirmwithCOD.search);
 router.post("/confirm", confirmwithCOD.confirm);
 router.post("/on_confirm", confirmwithCOD.on_confirm);
 router.post(["/on_status", "/on_update"], confirmwithCOD.on_status);
