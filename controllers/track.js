3b13d2a298aa1a7c4e09a5f74c30fb3f93930f89
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-09 12:21:28 +0530

    Update track.js

--- controllers/track.js
+++ controllers/track.js
@@ -3,21 +3,19 @@ const { validationResult } = require("express-validator");
 
 // Endpoint for handling track requests
 const track = async (req, res) => {
-  app.post("/track", (req, res) => {
-    // Extract the order ID and callback URL from the request body
-    const { order_id, callback_url } = req.body.message;
+  // Extract the order ID and callback URL from the request body
+  const { order_id, callback_url } = req.body.message;
 
-    // Perform necessary validations and checks on the order ID and callback URL
-    // ...
+  // Perform necessary validations and checks on the order ID and callback URL
+  // ...
 
-    // Call the track function with the order ID and callback URL
-    trackOrder(order_id, callback_url);
+  // Call the track function with the order ID and callback URL
+  trackOrder(order_id, callback_url);
 
-    // Return a success response
-    res.json({
-      success: true,
-      message: "Order tracking initiated successfully",
-    });
+  // Return a success response
+  res.json({
+    success: true,
+    message: "Order tracking initiated successfully",
   });
 };
 
