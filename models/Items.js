7a06a2cb07f57d656c8fa19c7bf20e5c185511f9
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-09 21:44:39 +0530

    Update Items.js

--- models/Items.js
+++ models/Items.js
@@ -13,6 +13,10 @@ const ItemSchema = new mongoose.Schema({
     type: Number,
     required: true,
   },
+  type: {
+    type: String,
+    required: true,
+  },
   rating: {
     type: Number,
     default: 0,
