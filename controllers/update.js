99230ffe92f03a26a967564672a3309d9e02414d
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-09 12:22:30 +0530

    Update update.js

--- controllers/update.js
+++ controllers/update.js
@@ -1,24 +1,35 @@
 const axios = require("axios");
 const { validationResult } = require("express-validator");
 
-// Endpoint for handling track requests
-const track = async (req, res) => {
-  app.post("/track", (req, res) => {
-    // Extract the order ID and callback URL from the request body
-    const { order_id, callback_url } = req.body.message;
+// Extract relevant data from the request
+const update = async (req, res) => {
+  const orderId = req.body.message.order.id;
+  const itemUpdates = req.body.message.order.items;
 
-    // Perform necessary validations and checks on the order ID and callback URL
-    // ...
+  // Process item updates
+  itemUpdates.forEach((itemUpdate) => {
+    const itemId = itemUpdate.id;
+    const returnQuantity = itemUpdate.quantity.count;
+    const reasonCode = itemUpdate.tags.reason_code;
+    const ttlApproval = itemUpdate.tags.ttl_approval;
+    const ttlReverseQC = itemUpdate.tags.ttl_reverseqc;
+    const images = itemUpdate.tags.image;
 
-    // Call the track function with the order ID and callback URL
-    trackOrder(order_id, callback_url);
+    // Perform necessary actions based on the item updates
+    // Here, you can update your seller records with the return information,
+    // handle the return process, update inventory, etc.
 
-    // Return a success response
-    res.json({
-      success: true,
-      message: "Order tracking initiated successfully",
-    });
+    console.log(`Order ID: ${orderId}`);
+    console.log(`Item ID: ${itemId}`);
+    console.log(`Return Quantity: ${returnQuantity}`);
+    console.log(`Reason Code: ${reasonCode}`);
+    console.log(`TTL Approval: ${ttlApproval}`);
+    console.log(`TTL Reverse QC: ${ttlReverseQC}`);
+    console.log(`Images: ${images}`);
   });
+
+  // Send a response back
+  res.status(200).json({ message: "Update received successfully" });
 };
 
-exports.track = track;
+exports.update = update;
