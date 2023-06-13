bc52e696b771866d0596330db6beea998c020b89
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-13 20:43:48 +0530

    Create Inventory.js

---
+++ routes/App/Inventory.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const auth = require("./../../middleWare/auth");
+const { check } = require("express-validator");
+
+// Controller
+const onboardingController = require("./../../controllers/App/onboardingController");
+
+const router = express.Router();
+
+// Register Seller
+router.post(
+  "/WhatsAppNumber",
+  [check("WhatsAppNumber", "WhatsAppNumber is Required").not().isEmpty()],
+  onboardingController.WhatsAppNumber
+);
+
+module.exports = router;
