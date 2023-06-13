eb5f0743dc9690e0f051934780127233ecc02f82
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-13 20:59:32 +0530

    Update Inventory.js

--- routes/App/Inventory.js
+++ routes/App/Inventory.js
@@ -3,15 +3,11 @@ const auth = require("./../../middleWare/auth");
 const { check } = require("express-validator");
 
 // Controller
-const onboardingController = require("./../../controllers/App/onboardingController");
+const InventoryController = require("./../../controllers/App/InventoryController");
 
 const router = express.Router();
 
 // Register Seller
-router.post(
-  "/WhatsAppNumber",
-  [check("WhatsAppNumber", "WhatsAppNumber is Required").not().isEmpty()],
-  onboardingController.WhatsAppNumber
-);
+router.get("/InventoryGet", InventoryController.InventoryGet);
 
 module.exports = router;
