eb51964ece87d9ae696f7883d42ea62fa267fcc5
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-19 20:55:04 +0530

    Update ProfileController.js

--- controllers/App/ProfileController.js
+++ controllers/App/ProfileController.js
@@ -33,6 +33,10 @@ const getStoreList = async (req, res, next) => {
 
     if (user) {
       res.status(202).json({ status: true, StoresList: user[0].Store });
+    } else {
+      res
+        .status(204)
+        .json({ status: false, message: "Number Does not Exists" });
     }
   } catch (error) {
     console.log(error);
@@ -50,6 +54,23 @@ const getStoreData = async (req, res, next) => {
   const { StoreID } = req.body;
 
   console.log("StoreID = " + StoreID);
+
+  let store;
+
+  try {
+    store = await Store.find({ _id: StoreID }, { StoreName: 1, StoreType: 1 });
+
+    if (store) {
+      res.status(202).json({ status: true, store });
+    } else {
+      res
+        .status(204)
+        .json({ status: false, message: "Number Does not Exists" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(304).json({ status: false, message: "Number Does not Exists" });
+  }
 };
 
 exports.getStoreData = getStoreData;
