cfc892eddfb27b455f8f80968ad5478b7d6d3111
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-23 23:01:59 +0530

    Update OrderControllers.js

--- controllers/App/OrderControllers.js
+++ controllers/App/OrderControllers.js
@@ -148,6 +148,18 @@ const orderDetails = async (req, res, next) => {
   }
 };
 
+const UpdateStatus = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { id, val } = req.body;
+
+  res.status(202).json({ status: true, id, val });
+};
+
 exports.AddOrder = AddOrder;
 exports.orderDetails = orderDetails;
+exports.UpdateStatus = UpdateStatus;
 exports.GetStoreOrder = GetStoreOrder;
