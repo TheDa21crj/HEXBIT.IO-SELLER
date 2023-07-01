904a0711b475053941c4ed6feddef0c7a76af6e7
Rishav Singh <rishavrajcrj@gmail.com>
2023-07-01 16:17:44 +0530

    Create search.js

---
+++ controllers/ONDC/search.js
@@ -0,0 +1,28 @@
+const { validationResult } = require("express-validator");
+
+// models
+const Seller = require("./../models/Seller");
+const Store = require("./../models/Store");
+const Items = require("./../models/Items");
+
+// Define the route for the /search API
+const cancel = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { context, message } = req.body;
+
+  let response = { context };
+
+  const responseData = await axios.post(process.env.SEARCH, response, {
+    headers: {
+      Authorization: process.env.Authorization,
+    },
+  });
+
+  res.status(202).json(responseData.data);
+};
+
+exports.cancel = cancel;
