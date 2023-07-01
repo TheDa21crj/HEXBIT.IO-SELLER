9734972aeb65064232f6059c2713b7a46473b61e
Rishav Singh <rishavrajcrj@gmail.com>
2023-07-01 16:21:06 +0530

    Update search.js

--- controllers/ONDC/search.js
+++ controllers/ONDC/search.js
@@ -1,28 +1,27 @@
 const { validationResult } = require("express-validator");
 
 // models
-const Seller = require("./../models/Seller");
-const Store = require("./../models/Store");
-const Items = require("./../models/Items");
+const Seller = require("./../../models/Seller");
+const Store = require("./../../models/Store");
+const Items = require("./../../models/Items");
 
 // Define the route for the /search API
 const search = async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
+  console.log(req);
 
-  const { context, message } = req.body;
-
-  let response = { context };
-
-  const responseData = await axios.post(process.env.SEARCH, response, {
-    headers: {
-      Authorization: process.env.Authorization,
+  res.status(202).json({
+    message: {
+      ack: {
+        status: "ACK",
+      },
+    },
+    error: {
+      type: "CONTEXT-ERROR",
+      code: "string",
+      path: "string",
+      message: "string",
     },
   });
-
-  res.status(202).json(responseData.data);
 };
 
 exports.search = search;
