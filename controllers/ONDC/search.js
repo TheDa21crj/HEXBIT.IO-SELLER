93e2a9b99b84225ee1083f4401460c22ac40d1f8
Rishav Singh <rishavrajcrj@gmail.com>
2023-07-01 17:54:51 +0530

    Update search.js

--- controllers/ONDC/search.js
+++ controllers/ONDC/search.js
@@ -10,8 +10,10 @@ const search = async (req, res) => {
   console.log(
     "------------------------------req.body------------------------------"
   );
+  console.log("req.body.message");
   console.log(req.body.message);
   console.log("------------------------------------------------------------");
+  console.log("req.body.context");
   console.log(req.body.context);
 
   res.status(200).json({
