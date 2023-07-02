3fe01e7f800c5328ddd6a7596534673c59f7bf10
Rishav Singh <rishavrajcrj@gmail.com>
2023-07-02 07:50:57 +0530

    Update search.js

--- controllers/ONDC/search.js
+++ controllers/ONDC/search.js
@@ -11,8 +11,16 @@ const search = async (req, res) => {
     "------------------------------req.body------------------------------"
   );
   console.log("req.body.message");
-  console.log(req.body.message.intent);
+  console.log(
+    "Payment Type" +
+      req.body.message.intent.payment["@ondc/org/buyer_app_finder_fee_type"]
+  );
+  console.log(
+    "Payment Amount" +
+      req.body.message.intent.payment["@ondc/org/buyer_app_finder_fee_amount"]
+  );
 
+  console.log("Location -> " + req.body.message.intent.fulfillment.location);
   // console.log("Delivery TYPE -> " + req.body.message.intent.fulfillment.type);
   // console.log("Search Word -> ", req.body.message.intent.item.descriptor.name);
 
