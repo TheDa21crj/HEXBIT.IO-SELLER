7d32b5beba18c6d0a772a3af695798380e22df17
Rishav Singh <rishavrajcrj@gmail.com>
2023-07-04 08:52:18 +0530

    Update search.js

--- controllers/ONDC/search.js
+++ controllers/ONDC/search.js
@@ -8,10 +8,6 @@ const Items = require("./../../models/Items");
 
 // Define the route for the /search API
 const search = async (req, res) => {
-  console.log(
-    "------------------------------req.body------------------------------"
-  );
-
   const { context, message } = req.body;
 
   const response = {
@@ -34,37 +30,45 @@ const search = async (req, res) => {
     },
   };
 
-  const responseData = await axios.post(
-    "https://virtserver.swaggerhub.com/ONDCTech/ONDC-Protocol-Core/1.0.0/on_search",
-    response,
-    {
-      headers: {
-        Authorization: process.env.Authorization,
+  try {
+    const responseData = await axios.post(
+      // "https://virtserver.swaggerhub.com/ONDCTech/ONDC-Protocol-Core/1.0.0/on_search",
+      "https://buyer-app.ondc.org/protocol/v1/on_search",
+      response,
+      {
+        headers: {
+          Authorization: process.env.Authorization,
+        },
+      }
+    );
+
+    console.log(responseData);
+
+    // console.log("Payment Type -> " + message.intent.payment["@ondc/org/buyer_app_finder_fee_type"]);
+    // console.log("Payment Amount -> " + message.intent.payment["@ondc/org/buyer_app_finder_fee_amount"]);
+    // console.log("GPS -> " + message.intent.fulfillment.end.location.gps);
+    // console.log("Delivery TYPE -> " + message.intent.fulfillment.type);
+    // console.log("Search Word -> ", message.intent.item.descriptor.name);
+
+    res.status(200).json({
+      message: {
+        ack: {
+          status: "ACK",
+        },
       },
-    }
-  );
-
-  console.log(responseData);
-
-  // console.log("Payment Type -> " + message.intent.payment["@ondc/org/buyer_app_finder_fee_type"]);
-  // console.log("Payment Amount -> " + message.intent.payment["@ondc/org/buyer_app_finder_fee_amount"]);
-  // console.log("GPS -> " + message.intent.fulfillment.end.location.gps);
-  // console.log("Delivery TYPE -> " + message.intent.fulfillment.type);
-  // console.log("Search Word -> ", message.intent.item.descriptor.name);
-
-  res.status(200).json({
-    message: {
-      ack: {
-        status: "ACK",
+      error: {
+        type: "CONTEXT-ERROR",
+        code: "string",
+        path: "string",
+        message: "string",
       },
-    },
-    error: {
-      type: "CONTEXT-ERROR",
-      code: "string",
-      path: "string",
-      message: "string",
-    },
-  });
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      message: "error",
+    });
+  }
 };
 
 exports.search = search;
