5ec296cdf11393e129cc062c6322e02b7f090888
Rishav Singh <rishavrajcrj@gmail.com>
2023-07-13 03:03:24 +0530

    Update handlers.js

--- handlers.js
+++ handlers.js
@@ -3,6 +3,9 @@ const ondc = require("ondc-node");
 const onSearch = async (req, res) => {
   // Use the req.body to find the result of search
   let result = await Search(req.body);
+
+  console.log(result);
+
   // The result should be in correct schema as per ONDC spec
   res.status(200).send(result);
 };
