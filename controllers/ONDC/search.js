487eda37cf5abf009a537d265027a1b85d310047
Rishav Singh <rishavrajcrj@gmail.com>
2023-07-12 09:38:55 +0530

    Update search.js

--- controllers/ONDC/search.js
+++ controllers/ONDC/search.js
@@ -99,81 +99,59 @@ const search = async (req, res) => {
 
   const signingString = `created:${createdTimestamp}\nexpires:${expirationDurationInSeconds}\ndigest:${digest}`;
 
-  const authorizationHeader = `Signature keyId="techondc.hexbit.io",algorithm="ed25519",created="${createdTimestamp}",expires="${expirationDurationInSeconds}",headers="(created) (expires) digest",signature="${digest}"`;
+  const authorizationHeader = `Signature keyId="techondc.hexbit.io",algorithm="ed25519",created="${createdTimestamp}",expires="${expiresTimestamp}",headers="(created) (expires) digest",signature="${digest}"`;
   console.log("Authorization Header:", authorizationHeader);
 
-  res.status(200).json({
-    message: {
-      ack: {
-        status: "ACK",
+  try {
+    const responseData = await axios.post(
+      "https://pilot-gateway-1.beckn.nsdl.co.in/on_search",
+      response,
+      {
+        headers: {
+          // Authorization: process.env.Authorization,
+          "X-Gateway-Authorization": {
+            authorizationHeader,
+          },
+        },
+      }
+    );
+
+    console.log(
+      "-----------------------------responseData.data.message-----------------------------"
+    );
+
+    console.log(responseData.data);
+    console.log(responseData.data.message);
+
+    // console.log("Payment Type -> " + message.intent.payment["@ondc/org/buyer_app_finder_fee_type"]);
+    // console.log("Payment Amount -> " + message.intent.payment["@ondc/org/buyer_app_finder_fee_amount"]);
+    // console.log("GPS -> " + message.intent.fulfillment.end.location.gps);
+    // console.log("Delivery TYPE -> " + message.intent.fulfillment.type);
+    // console.log("Search Word -> ", message.intent.item.descriptor.name);
+
+    res.status(200).json({
+      message: {
+        ack: {
+          status: "ACK",
+        },
       },
-    },
-  });
-
-  // try {
-  //   const responseData = await axios.post(
-  //     "https://pilot-gateway-1.beckn.nsdl.co.in/on_search",
-  //     response,
-  //     {
-  //       headers: {
-  //         // Authorization: process.env.Authorization,
-  //         "X-Gateway-Authorization": {
-  //           "Signature keyId": "example-bg.com|bg3456|ed25519",
-  //           algorithm: "ed25519",
-  //           created: "1641287885",
-  //           expires: "1641287885",
-  //           headers: "(created) (expires) digest",
-  //           signature:
-  //             "hJ5sCmbe7s9Wateq6QAdBGloVSkLuLHWOXcRkzrMcVLthFldV4gnT9Vrnq9iDNPVSKuDqaercVjQwFlj0Ml+3Q==",
-  //           type: "BPP",
-  //           token: process.env.Authorization,
-  //           valid_from: "2023-05-18T10:10:08.196Z",
-  //           valid_to: "2026-05-18T10:10:08.196Z",
-  //           status: "SUBSCRIBED",
-  //           signing_public_key: "iUTpWtF68yckymVVY/aaXPHrMMPRz/dvYhXf3leVRI8=",
-  //           encr_public_key:
-  //             "MCowBQYDK2VuAyEAV4FZ4Px1z9ZUbKGTjvGrddnzqv8XCgjy28a9048cJ0M=",
-  //         },
-  //       },
-  //     }
-  //   );
-
-  //   console.log(
-  //     "-----------------------------responseData.data.message-----------------------------"
-  //   );
-
-  //   console.log(responseData.data);
-  //   console.log(responseData.data.message);
-
-  //   // console.log("Payment Type -> " + message.intent.payment["@ondc/org/buyer_app_finder_fee_type"]);
-  //   // console.log("Payment Amount -> " + message.intent.payment["@ondc/org/buyer_app_finder_fee_amount"]);
-  //   // console.log("GPS -> " + message.intent.fulfillment.end.location.gps);
-  //   // console.log("Delivery TYPE -> " + message.intent.fulfillment.type);
-  //   // console.log("Search Word -> ", message.intent.item.descriptor.name);
-
-  //   res.status(200).json({
-  //     message: {
-  //       ack: {
-  //         status: "ACK",
-  //       },
-  //     },
-  //     error: {
-  //       type: null,
-  //       code: null,
-  //       path: null,
-  //       message: null,
-  //     },
-  //   });
-  // } catch (error) {
-  //   console.log(
-  //     "*************************************ERROR*************************************"
-  //   );
-  //   console.log(error.message);
-  //   res.status(404).json({
-  //     message: "error",
-  //     error: error,
-  //   });
-  // }
+      error: {
+        type: null,
+        code: null,
+        path: null,
+        message: null,
+      },
+    });
+  } catch (error) {
+    console.log(
+      "*************************************ERROR*************************************"
+    );
+    console.log(error.message);
+    res.status(404).json({
+      message: "error",
+      error: error,
+    });
+  }
 };
 
 exports.search = search;
